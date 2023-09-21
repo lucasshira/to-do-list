08c80c8be1e8758ab88c5dc2aabb2031f8b1aba7
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2023-09-20 22:23:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+document.getElementById('btn').addEventListener('click', function(){
+    let input = document.getElementById('input').value;
+    if(input === ""){
+        alert("Please enter something");
+    } else {
+
+    let elem = document.createElement('li');
+    let btn1 = document.createElement('button');
+    let btn2 = document.createElement('button');
+
+    btn1.innerText = "Done";
+    btn2.innerText = "Delete";
+
+    let ul = document.querySelector('ul');
+    ul.appendChild(elem);
+
+    elem.innerText = input;
+    elem.appendChild(btn1);
+    elem.appendChild(btn2);
+
+    btn1.style.border = "none";
+    btn1.style.marginLeft = "3%";
+    btn1.style.marginBottom = "8px";
+    btn1.style.borderRadius = "3px";
+    btn1.style.backgroundColor = "#b4b3d8";
+    btn1.style.padding = "2px";
+
+    btn2.style.border = "none";
+    btn2.style.marginLeft = "3%";
+    btn2.style.marginBottom = "8px";
+    btn2.style.borderRadius = "3px";
+    btn2.style.backgroundColor = "#b4b3d8";
+    btn2.style.padding = "2px";
+
+    btn1.addEventListener('click', function() {
+        elem.style.textDecoration = "line-through";
+    });
+
+    btn2.addEventListener('click', function() {
+        elem.remove();
+    });
+
+    document.getElementById('input').value="";
+    }
+});
