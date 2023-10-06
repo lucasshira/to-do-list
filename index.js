4ea6bd454c0e1c2abc77100cf82bbad18cfdb6c0
Lucas Gabriel <144144880+lucasshira@users.noreply.github.com>
2023-10-06 14:22:00 -0300

    Update index.js
    As tarefas colocadas na lista agora ficam salvas.

--- index.js
+++ index.js
@@ -1,69 +1,108 @@
-document.getElementById('btn').addEventListener('click', function(){
-        addTask();
+document.getElementById('btn').addEventListener('click', function () {
+    adicionarTarefa();
 });
 
 document.getElementById('input').addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
-        addTask();
+        adicionarTarefa();
     }
 });
 
 function formatDate(date) {
     const options = { day: '2-digit', month: '2-digit' };
     return date.toLocaleDateString(undefined, options);
-};
+}
 
-function addTask() {
-    let input = document.getElementById('input').value;
-    if(input === ""){
-        alert("Please enter something");
-    } else {
-
+function criarElementoTarefa(texto, data, concluida) {
+    const elem = document.createElement('li');
     const dateSpan = document.createElement('span');
-    const currentDate = new Date();
-    dateSpan.innerText = formatDate(currentDate);
+    dateSpan.innerText = data;
 
-    let elem = document.createElement('li');
-    let btn1 = document.createElement('button');
-    let btn2 = document.createElement('button');
-
-    btn1.innerText = "Done";
-    btn2.innerText = "Delete";
-
-    let ul = document.querySelector('ul');
-    ul.appendChild(elem);
+    const taskText = document.createElement('span');
+    taskText.innerText = " - " + texto;
+    if (concluida) {
+        elem.style.textDecoration = "line-through";
+    }
 
     elem.appendChild(dateSpan);
-
-    let taskText = document.createElement('span');
-    taskText.innerText = " - " + input;
     elem.appendChild(taskText);
 
-    elem.appendChild(btn1);
-    elem.appendChild(btn2);
-
+    const btn1 = document.createElement('button');
+    btn1.innerText = "Done";
     btn1.style.border = "none";
     btn1.style.marginLeft = "3%";
     btn1.style.marginBottom = "8px";
     btn1.style.borderRadius = "3px";
     btn1.style.backgroundColor = "#b4b3d8";
     btn1.style.padding = "2px";
+    btn1.addEventListener('click', function () {
+        elem.style.textDecoration = "line-through";
+        salvarTarefas();
+    });
 
+    const btn2 = document.createElement('button');
+    btn2.innerText = "Delete";
     btn2.style.border = "none";
     btn2.style.marginLeft = "3%";
     btn2.style.marginBottom = "8px";
     btn2.style.borderRadius = "3px";
     btn2.style.backgroundColor = "#b4b3d8";
     btn2.style.padding = "2px";
-
-    btn1.addEventListener('click', function() {
-        elem.style.textDecoration = "line-through";
-    });
-
-    btn2.addEventListener('click', function() {
+    btn2.addEventListener('click', function () {
         elem.remove();
+        salvarTarefas();
     });
 
-    document.getElementById('input').value="";
+    elem.appendChild(btn1);
+    elem.appendChild(btn2);
+
+    return elem;
+}
+
+function adicionarTarefa() {
+    let input = document.getElementById('input').value;
+    if (input === "") {
+        alert("Please enter something");
+    } else {
+        const currentDate = new Date();
+        const date = formatDate(currentDate);
+        const elem = criarElementoTarefa(input, date, false);
+
+        const ul = document.querySelector('ul');
+        ul.appendChild(elem);
+
+        document.getElementById('input').value = "";
+
+        salvarTarefas();
     }
-};
+}
+
+function salvarTarefas() {
+    const tarefas = [];
+    const listaTarefas = document.querySelectorAll('ul > li');
+
+    listaTarefas.forEach((item) => {
+        const data = item.querySelector('span:first-child').innerText;
+        const texto = item.querySelector('span:nth-child(2)').innerText.slice(3);
+        const concluida = item.style.textDecoration === "line-through";
+
+        tarefas.push({ data, texto, concluida });
+    });
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+
+function carregarTarefasSalvas() {
+    const tarefasSalvas = localStorage.getItem('tarefas');
+    if (tarefasSalvas) {
+        const tarefas = JSON.parse(tarefasSalvas);
+
+        tarefas.forEach((tarefa) => {
+            const elem = criarElementoTarefa(tarefa.texto, tarefa.data, tarefa.concluida);
+            const ul = document.querySelector('ul');
+            ul.appendChild(elem);
+        });
+    }
+}
+
+carregarTarefasSalvas();
